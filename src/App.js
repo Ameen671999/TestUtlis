bcbd7c43cb41e5434b724d2bb5dfc2cf97ad5a41
Ameen671999 <ameen671999@gmail.com>
2021-10-23 15:54:46 +0530

    4.Dark Mode & Refactoring App Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import Navbar from './components/Navbar'
-// import TextForm from './components/TextForm'
-import PropTypes from 'prop-types'
-import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import PropTypes from "prop-types";
+import { useState } from "react/cjs/react.development";
+// import About from "./components/About";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743"
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white"
+    }
+  };
   return (
     <>
-      <Navbar title="TextUtlis" aboutText="About Us" />
+      <Navbar title="TextUtlis" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-      {/* <TextForm heading = 'Enter a text to analyze' /> */}
-      <About/>
+        <TextForm mode={mode} heading="Enter a text to analyze"/>
+        {/* <About/> */}
       </div>
     </>
   );
@@ -18,12 +30,13 @@ function App() {
 
 Navbar.prototype = {
   title: PropTypes.string,
-  aboutText: PropTypes.string
-}
+  aboutText: PropTypes.string,
+};
 
-Navbar.defaultProps = { // value is not given
-  title: 'set title',
-  aboutText: 'About'
-}
+Navbar.defaultProps = {
+  // value is not given
+  title: "set title",
+  aboutText: "About",
+};
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,14 +29,26 @@ export default function TextForm(props) {
     setText(str);
   };
 
+  const handleCopyClick = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleSpaceClick = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
 
   return (
     <>
-      <div className="container">
+      <div className="container" style = {{color: props.mode === 'dark' ? 'white' : 'black'}}>
         <div className="mb-3">
           <h1> {props.heading} </h1>
           <textarea
+          style = {{backgroundColor: props.mode === 'dark' ? '#042743' : 'light'}}
             className="form-control"
             value={text}
             onChange={handleOnchange}
@@ -44,27 +56,38 @@ export default function TextForm(props) {
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
-          Convert Uppercase
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
-          Convert LowerCase
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
-          Clear Text
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleCaptilizeClick}>
-          Captilized Text
-        </button>
+        <div className="container" >
+          <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+            Convert Uppercase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+            Convert LowerCase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+            Clear Text
+          </button>
+          <button
+            className="btn btn-primary mx-2"
+            onClick={handleCaptilizeClick}
+          >
+            Captilized Text
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleCopyClick}>
+            Copy Text
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleSpaceClick}>
+            Remove Extra Spaces
+          </button>
+        </div>
       </div>
-      <div className="container my-2">
+      <div className="container my-2" style = {{color: props.mode === 'dark' ? 'white' : 'black'}}>
         <h2> Your text</h2>
         <p>
           {text.split(" ").length} words, {text.length} characters
         </p>
         <p>Minutes to read {text.split(" ").length * 0.008}</p>
         <h3>Preview</h3>
-        <p> {text} </p>
+        <p> {text.length>0?text:"Enter something in text above"} </p>
       </div>
     </>
   );
